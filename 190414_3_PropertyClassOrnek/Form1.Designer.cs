9ab042881cb88b15f427db7a3c42952130cdb89f
Hasan BİLGİN <hasanbilgintr@hotmail.com>
2019-04-14 12:42:59 +0300

    Add project files.

---
+++ 190414_3_PropertyClassOrnek/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace _190414_3_PropertyClassOrnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxMarka = new System.Windows.Forms.ComboBox();
+            this.comboBoxModel = new System.Windows.Forms.ComboBox();
+            this.comboBoxYakit = new System.Windows.Forms.ComboBox();
+            this.maskedTextBoxModelYili = new System.Windows.Forms.MaskedTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBoxFiyat = new System.Windows.Forms.TextBox();
+            this.Ekle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Marka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Yakıt Cinsi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Model Tarihi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Fiyat";
+            // 
+            // comboBoxMarka
+            // 
+            this.comboBoxMarka.FormattingEnabled = true;
+            this.comboBoxMarka.Location = new System.Drawing.Point(103, 13);
+            this.comboBoxMarka.Name = "comboBoxMarka";
+            this.comboBoxMarka.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMarka.TabIndex = 1;
+            // 
+            // comboBoxModel
+            // 
+            this.comboBoxModel.FormattingEnabled = true;
+            this.comboBoxModel.Location = new System.Drawing.Point(103, 47);
+            this.comboBoxModel.Name = "comboBoxModel";
+            this.comboBoxModel.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxModel.TabIndex = 1;
+            // 
+            // comboBoxYakit
+            // 
+            this.comboBoxYakit.FormattingEnabled = true;
+            this.comboBoxYakit.Location = new System.Drawing.Point(103, 81);
+            this.comboBoxYakit.Name = "comboBoxYakit";
+            this.comboBoxYakit.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxYakit.TabIndex = 1;
+            // 
+            // maskedTextBoxModelYili
+            // 
+            this.maskedTextBoxModelYili.Location = new System.Drawing.Point(103, 115);
+            this.maskedTextBoxModelYili.Mask = "00/00/0000";
+            this.maskedTextBoxModelYili.Name = "maskedTextBoxModelYili";
+            this.maskedTextBoxModelYili.Size = new System.Drawing.Size(121, 20);
+            this.maskedTextBoxModelYili.TabIndex = 2;
+            this.maskedTextBoxModelYili.ValidatingType = typeof(System.DateTime);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(258, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(156, 212);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textBoxFiyat
+            // 
+            this.textBoxFiyat.Location = new System.Drawing.Point(103, 153);
+            this.textBoxFiyat.Name = "textBoxFiyat";
+            this.textBoxFiyat.Size = new System.Drawing.Size(121, 20);
+            this.textBoxFiyat.TabIndex = 4;
+            // 
+            // Ekle
+            // 
+            this.Ekle.Location = new System.Drawing.Point(103, 201);
+            this.Ekle.Name = "Ekle";
+            this.Ekle.Size = new System.Drawing.Size(121, 23);
+            this.Ekle.TabIndex = 5;
+            this.Ekle.Text = "Ekle";
+            this.Ekle.UseVisualStyleBackColor = true;
+            this.Ekle.Click += new System.EventHandler(this.Ekle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 259);
+            this.Controls.Add(this.Ekle);
+            this.Controls.Add(this.textBoxFiyat);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.maskedTextBoxModelYili);
+            this.Controls.Add(this.comboBoxYakit);
+            this.Controls.Add(this.comboBoxModel);
+            this.Controls.Add(this.comboBoxMarka);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxMarka;
+        private System.Windows.Forms.ComboBox comboBoxModel;
+        private System.Windows.Forms.ComboBox comboBoxYakit;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxModelYili;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxFiyat;
+        private System.Windows.Forms.Button Ekle;
+    }
+}
+
